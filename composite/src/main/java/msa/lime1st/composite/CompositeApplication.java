cdc5e851a4ceeba0cc32ec7091314320e88d1b78
lime1st <lime1st@gmail.com>
2025-04-19 17:36:12 +0900

    chore(#1): init project

---
+++ composite/src/main/java/msa/lime1st/composite/CompositeApplication.java
@@ -0,0 +1,13 @@
+package msa.lime1st.composite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompositeApplication.class, args);
+    }
+
+}
