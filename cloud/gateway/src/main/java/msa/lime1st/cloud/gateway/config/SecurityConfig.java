6e628b43b0922d3186a0c986d9096f6a743eca96
lime1st <lime1st@gmail.com>
2025-04-24 16:36:25 +0900

    feat: add security for gateway - oauth2 path added

---
+++ cloud/gateway/src/main/java/msa/lime1st/cloud/gateway/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package msa.lime1st.cloud.gateway.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        http
+            .csrf(CsrfSpec::disable)
+            .authorizeExchange(authorizeExchange -> authorizeExchange
+            .pathMatchers("/headerrouting/**").permitAll()
+            .pathMatchers("/actuator/**").permitAll()
+            .pathMatchers("/eureka/**").permitAll()
+            .pathMatchers("/oauth2/**").permitAll()
+            .pathMatchers("/login/**").permitAll()
+            .pathMatchers("/error/**").permitAll()
+            .pathMatchers("/openapi/**").permitAll()
+            .pathMatchers("/webjars/**").permitAll()
+            .anyExchange().authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+
+        return http.build();
+    }
+}
