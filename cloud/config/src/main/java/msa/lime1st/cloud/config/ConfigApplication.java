b045ad6d7a2465ae36f92a46b4fbd4a5bf583d15
lime1st <lime1st@gmail.com>
2025-04-28 18:05:28 +0900

    chore(#12): modify config repo

--- cloud/config/src/main/java/msa/lime1st/cloud/config/ConfigApplication.java
+++ cloud/config/src/main/java/msa/lime1st/cloud/config/ConfigApplication.java
@@ -2,10 +2,12 @@ package msa.lime1st.cloud.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -15,10 +17,15 @@ public class ConfigApplication {
     private static final Logger log = LoggerFactory.getLogger(ConfigApplication.class);
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(ConfigApplication.class, args);
-
-        String repoLocation = ctx.getEnvironment().getProperty("spring.cloud.config.server.native.searchLocations");
-        log.info("Search locations: {}", repoLocation);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner applicationRunner(ConfigurableApplicationContext ctx) {
+        return args -> {
+            String repoLocation = ctx.getEnvironment()
+                .getProperty("spring.cloud.config.server.native.search-locations[0]");
+            log.info("Search locations: '{}'", repoLocation);
+        };
+    }
 }
